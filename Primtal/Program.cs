37e25e4b379d5e1175fa6f8f813e521078b13964
Emillarsen194 <47022984+Emillarsen194@users.noreply.github.com>
2019-03-18 12:07:46 +0100

    Add project files.

---
+++ Primtal/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primtal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> primtal = new Queue<int>();
+
+            primtal.Enqueue(2);
+            primtal.Enqueue(3);
+            primtal.Enqueue(5);
+            primtal.Enqueue(7);
+            primtal.Enqueue(11);
+
+            
+
+            while (primtal.Count > 0)
+            {
+                int total = primtal.Dequeue();
+                Console.WriteLine(total);
+            }
+        }
+    }
+}
